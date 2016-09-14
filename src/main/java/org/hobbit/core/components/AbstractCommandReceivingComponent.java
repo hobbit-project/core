3d1a75bbc07d1e3af34563132f8a3014c7905efe
MichaelRoeder <michaasresearcher@gmail.com>
2016-09-14 15:10:09 +0200

    Further implementation of the abstract benchmark controller. Extended the command receiving component to use the start and stop commands for starting and stopping docker containers.

--- src/main/java/org/hobbit/core/components/AbstractCommandReceivingComponent.java
+++ src/main/java/org/hobbit/core/components/AbstractCommandReceivingComponent.java
@@ -4,115 +4,207 @@ import java.io.IOException;
 import java.nio.ByteBuffer;
 
 import org.apache.commons.io.Charsets;
+import org.hobbit.core.Commands;
 import org.hobbit.core.Constants;
 import org.hobbit.core.data.StartCommandData;
+import org.hobbit.core.data.StopCommandData;
+import org.hobbit.core.rabbit.RabbitMQUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.google.gson.Gson;
 import com.rabbitmq.client.AMQP;
+import com.rabbitmq.client.AMQP.BasicProperties;
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Consumer;
 import com.rabbitmq.client.DefaultConsumer;
 import com.rabbitmq.client.Envelope;
+import com.rabbitmq.client.QueueingConsumer;
 
 public abstract class AbstractCommandReceivingComponent extends AbstractComponent implements CommandReceivingComponent {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(AbstractCommandReceivingComponent.class);
+	private static final Logger LOGGER = LoggerFactory.getLogger(AbstractCommandReceivingComponent.class);
 
-    protected Channel cmdChannel = null;
-    private String containerId;
-    /**
-     * Threadsafe JSON parser.
-     */
-    private Gson gson = new Gson();
+	protected Channel cmdChannel = null;
+	private String containerId;
+	private String responseQueueName = null;
+	private QueueingConsumer responseConsumer = null;
+	/**
+	 * Threadsafe JSON parser.
+	 */
+	private Gson gson = new Gson();
 
-    @Override
-    public void init() throws Exception {
-        super.init();
-        cmdChannel = connection.createChannel();
-        String queueName = cmdChannel.queueDeclare().getQueue();
-        cmdChannel.exchangeDeclare(Constants.HOBBIT_COMMAND_EXCHANGE_NAME, "fanout");
-        cmdChannel.queueBind(queueName, Constants.HOBBIT_COMMAND_EXCHANGE_NAME, "");
+	@Override
+	public void init() throws Exception {
+		super.init();
+		cmdChannel = connection.createChannel();
+		String queueName = cmdChannel.queueDeclare().getQueue();
+		cmdChannel.exchangeDeclare(Constants.HOBBIT_COMMAND_EXCHANGE_NAME, "fanout");
+		cmdChannel.queueBind(queueName, Constants.HOBBIT_COMMAND_EXCHANGE_NAME, "");
 
-        Consumer consumer = new DefaultConsumer(cmdChannel) {
-            @Override
-            public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties,
-                    byte[] body) throws IOException {
-                handleCmd(body);
-            }
-        };
-        cmdChannel.basicConsume(queueName, true, consumer);
+		Consumer consumer = new DefaultConsumer(cmdChannel) {
+			@Override
+			public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties,
+					byte[] body) throws IOException {
+				handleCmd(body, properties.getReplyTo());
+			}
+		};
+		cmdChannel.basicConsume(queueName, true, consumer);
 
-        if (System.getenv().containsKey(Constants.HOBBIT_SESSION_ID_KEY)) {
-            containerId = System.getenv().get(Constants.HOBBIT_SESSION_ID_KEY);
-        }
-        if (containerId == null) {
-            LOGGER.info("Couldn't get the id of this Docker container. Won't be able to create containers.");
-        }
-    }
+		if (System.getenv().containsKey(Constants.HOBBIT_SESSION_ID_KEY)) {
+			containerId = System.getenv().get(Constants.HOBBIT_SESSION_ID_KEY);
+		}
+		if (containerId == null) {
+			LOGGER.info("Couldn't get the id of this Docker container. Won't be able to create containers.");
+		}
+	}
 
-    protected void sendToCmdQueue(byte command) throws IOException {
-        sendToCmdQueue(command, null);
-    }
+	/**
+	 * Sends the given command to the command queue.
+	 * 
+	 * @param command
+	 *            the command that should be sent
+	 * @throws IOException
+	 */
+	protected void sendToCmdQueue(byte command) throws IOException {
+		sendToCmdQueue(command, null);
+	}
 
-    protected void sendToCmdQueue(byte command, byte data[]) throws IOException {
-        byte sessionIdBytes[] = getHobbitSessionId().getBytes(Charsets.UTF_8);
-        // + 5 because 4 bytes for the session ID length and 1 byte for the
-        // command
-        int dataLength = sessionIdBytes.length + 5;
-        boolean attachData = (data != null) && (data.length > 0);
-        if (attachData) {
-            dataLength += data.length;
-        }
-        ByteBuffer buffer = ByteBuffer.allocate(dataLength);
-        buffer.putInt(sessionIdBytes.length);
-        buffer.put(sessionIdBytes);
-        buffer.put(command);
-        if (attachData) {
-            buffer.put(data);
-        }
-        cmdChannel.basicPublish(Constants.HOBBIT_COMMAND_EXCHANGE_NAME, "", null, buffer.array());
-    }
+	/**
+	 * Sends the given command to the command queue with the given data
+	 * appended.
+	 * 
+	 * @param command
+	 *            the command that should be sent
+	 * @param data
+	 *            data that should be appended to the command
+	 * @throws IOException
+	 */
+	protected void sendToCmdQueue(byte command, byte data[]) throws IOException {
+		sendToCmdQueue(command, data, null);
+	}
 
-    protected void handleCmd(byte bytes[]) {
-        ByteBuffer buffer = ByteBuffer.wrap(bytes);
-        int idLength = buffer.getInt();
-        byte sessionIdBytes[] = new byte[idLength];
-        buffer.get(sessionIdBytes);
-        String sessionId = new String(sessionIdBytes, Charsets.UTF_8);
-        if (sessionId.equals(getHobbitSessionId())) {
-            byte command = buffer.get();
-            byte remainingData[];
-            if (buffer.remaining() > 0) {
-                remainingData = new byte[buffer.remaining()];
-                buffer.get(remainingData);
-            } else {
-                remainingData = new byte[0];
-            }
-            receiveCommand(command, remainingData);
-        }
-    }
+	/**
+	 * Sends the given command to the command queue with the given data appended
+	 * and using the given properties.
+	 * 
+	 * @param command
+	 *            the command that should be sent
+	 * @param data
+	 *            data that should be appended to the command
+	 * @param props
+	 *            properties that should be used for the message
+	 * @throws IOException
+	 */
+	protected void sendToCmdQueue(byte command, byte data[], BasicProperties props) throws IOException {
+		byte sessionIdBytes[] = getHobbitSessionId().getBytes(Charsets.UTF_8);
+		// + 5 because 4 bytes for the session ID length and 1 byte for the
+		// command
+		int dataLength = sessionIdBytes.length + 5;
+		boolean attachData = (data != null) && (data.length > 0);
+		if (attachData) {
+			dataLength += data.length;
+		}
+		ByteBuffer buffer = ByteBuffer.allocate(dataLength);
+		buffer.putInt(sessionIdBytes.length);
+		buffer.put(sessionIdBytes);
+		buffer.put(command);
+		if (attachData) {
+			buffer.put(data);
+		}
+		cmdChannel.basicPublish(Constants.HOBBIT_COMMAND_EXCHANGE_NAME, "", props, buffer.array());
+	}
 
-    protected String createContainer(String dataGeneratorImageName, String[] envVariables) {
-        gson.toJson(new StartCommandData(dataGeneratorImageName, "", containerId, envVariables));
-        // TODO
-        return;
-    }
+	protected void handleCmd(byte bytes[], String replyTo) {
+		ByteBuffer buffer = ByteBuffer.wrap(bytes);
+		int idLength = buffer.getInt();
+		byte sessionIdBytes[] = new byte[idLength];
+		buffer.get(sessionIdBytes);
+		String sessionId = new String(sessionIdBytes, Charsets.UTF_8);
+		if (sessionId.equals(getHobbitSessionId())) {
+			byte command = buffer.get();
+			byte remainingData[];
+			if (buffer.remaining() > 0) {
+				remainingData = new byte[buffer.remaining()];
+				buffer.get(remainingData);
+			} else {
+				remainingData = new byte[0];
+			}
+			receiveCommand(command, remainingData);
+		}
+	}
 
-    protected String stopContainer(String containerId, String[] envVariables) {
-        // TODO Auto-generated method stub
-    }
+	/**
+	 * This method sends a {@link Commands#DOCKER_CONTAINER_START} command to
+	 * create and start an instance of the given image using the given
+	 * environment variables.
+	 * 
+	 * @param imageName
+	 *            the name of the image of the docker container
+	 * @param envVariables
+	 *            environment variables that should be added to the created
+	 *            container
+	 * @return the id of the container or null if an error occurred
+	 */
+	protected String createContainer(String imageName, String[] envVariables) {
+		try {
+			initResponseQueue();
+			byte data[] = RabbitMQUtils.writeString(gson.toJson(new StartCommandData(imageName, "", containerId,
+					envVariables)));
+			BasicProperties props = new BasicProperties.Builder().deliveryMode(2).replyTo(responseQueueName).build();
+			sendToCmdQueue(Commands.DOCKER_CONTAINER_START, data, props);
+			QueueingConsumer.Delivery delivery = responseConsumer.nextDelivery();
+			if (delivery.getBody().length > 0) {
+				return RabbitMQUtils.readString(delivery.getBody());
+			}
+		} catch (Exception e) {
+			LOGGER.error("Got exception while trying to request the creation of an instance of the \"" + imageName
+					+ "\" image.", e);
+		}
+		return null;
+	}
 
-    @Override
-    public void close() throws IOException {
-        if (cmdChannel != null) {
-            try {
-                cmdChannel.close();
-            } catch (Exception e) {
-            }
-        }
-        super.close();
-    }
+	/**
+	 * This method sends a {@link Commands#DOCKER_CONTAINER_STOP} command to
+	 * stop the container with the given id.
+	 * 
+	 * @param containerId
+	 *            the id of the container that should be stopped
+	 */
+	protected void stopContainer(String containerId) {
+		byte data[] = RabbitMQUtils.writeString(gson.toJson(new StopCommandData(containerId)));
+		try {
+			sendToCmdQueue(Commands.DOCKER_CONTAINER_STOP, data);
+		} catch (IOException e) {
+			LOGGER.error("Got exception while trying to stop the container with the id\"" + containerId + "\".", e);
+		}
+	}
+
+	/**
+	 * Internal method for initializing the {@link #responseQueueName} and the
+	 * {@link #responseConsumer} if they haven't been initialized before.
+	 * 
+	 * @throws IOException
+	 */
+	private void initResponseQueue() throws IOException {
+		if (responseQueueName == null) {
+			responseQueueName = cmdChannel.queueDeclare().getQueue();
+		}
+		if (responseConsumer == null) {
+			responseConsumer = new QueueingConsumer(cmdChannel);
+			cmdChannel.basicConsume(responseQueueName, responseConsumer);
+		}
+	}
+
+	@Override
+	public void close() throws IOException {
+		if (cmdChannel != null) {
+			try {
+				cmdChannel.close();
+			} catch (Exception e) {
+			}
+		}
+		super.close();
+	}
 
 }
--- src/main/java/org/hobbit/core/rabbit/RabbitMQUtils.java
+++ src/main/java/org/hobbit/core/rabbit/RabbitMQUtils.java
@@ -1,8 +1,14 @@
 package org.hobbit.core.rabbit;
 
+import java.io.StringReader;
+import java.io.StringWriter;
 import java.nio.ByteBuffer;
 
 import org.apache.commons.io.Charsets;
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.ModelFactory;
+import org.apache.jena.riot.Lang;
+import org.apache.jena.riot.RDFDataMgr;
 
 /**
  * Contains utility methods for working with RabbitMQ.
@@ -12,65 +18,102 @@ import org.apache.commons.io.Charsets;
  */
 public class RabbitMQUtils {
 
-    /**
-     * Transforms the given byte data into a String using the UTF-8 encoding.
-     * 
-     * @param data
-     *            the byte array that should be transformed
-     * @return the String or null if the given byte array is null
-     */
-    public static String readString(byte[] data) {
-        if (data == null) {
-            return null;
-        } else {
-            return new String(data, Charsets.UTF_8);
-        }
-    }
+	public static final Lang DEFAULT_RDF_LANG = Lang.JSONLD;
 
-    /**
-     * Transforms the given buffer data into a String using the UTF-8 encoding.
-     * 
-     * @param buffer
-     *            the buffer that contains the byte data of the String
-     * @return the String or null if the given buffer is null
-     */
-    public static String readString(ByteBuffer buffer) {
-        if (buffer == null) {
-            return null;
-        } else {
-            return new String(readByteArray(buffer), Charsets.UTF_8);
-        }
-    }
+	/**
+	 * Reads a byte array from the given buffer assuming that it is preceded by
+	 * an int value containing the length of the byte array.
+	 * 
+	 * @param buffer
+	 *            the buffer containing an int containing the length of the byte
+	 *            array followed by the byte array itself
+	 * @return the byte array or null if the given byte array is null
+	 */
+	public static byte[] readByteArray(ByteBuffer buffer) {
+		if (buffer == null) {
+			return null;
+		} else {
+			int length = buffer.getInt();
+			byte[] data = new byte[length];
+			buffer.get(data, 0, data.length);
+			return data;
+		}
+	}
 
-    /**
-     * Reads a byte array from the given buffer assuming that it is preceded by
-     * an int value containing the length of the byte array.
-     * 
-     * @param buffer
-     *            the buffer containing an int containing the length of the byte
-     *            array followed by the byte array itself
-     * @return the byte array or null if the given byte array is null
-     */
-    public static byte[] readByteArray(ByteBuffer buffer) {
-        if (buffer == null) {
-            return null;
-        } else {
-            int length = buffer.getInt();
-            byte[] data = new byte[length];
-            buffer.get(data, 0, data.length);
-            return data;
-        }
-    }
+	/**
+	 * Reads an RDF model from the given byte array.
+	 * 
+	 * @param data
+	 *            the byte array containing the serialized RDF model
+	 * @return the deserialized model
+	 */
+	public static Model readModel(byte data[]) {
+		StringReader reader = new StringReader(readString(data));
+		Model model = ModelFactory.createDefaultModel();
+		RDFDataMgr.read(model, reader, "", DEFAULT_RDF_LANG);
+		return model;
+	}
 
-    /**
-     * Creates a byte array representation of the given String using UTF-8
-     * encoding.
-     * 
-     * @param string
-     *            the String that should be transformed into a byte array
-     * @return the UTF-8 byte array of the given String
-     */
-    public static byte[] writeString(String string) {
-        return string.getBytes(Charsets.UTF_8);
-    }
+	/**
+	 * Transforms the given byte data into a String using the UTF-8 encoding.
+	 * 
+	 * @param data
+	 *            the byte array that should be transformed
+	 * @return the String or null if the given byte array is null
+	 */
+	public static String readString(byte[] data) {
+		if (data == null) {
+			return null;
+		} else {
+			return new String(data, Charsets.UTF_8);
+		}
+	}
+
+	/**
+	 * Transforms the given buffer data into a String using the UTF-8 encoding.
+	 * 
+	 * @param buffer
+	 *            the buffer that contains the byte data of the String
+	 * @return the String or null if the given buffer is null
+	 */
+	public static String readString(ByteBuffer buffer) {
+		if (buffer == null) {
+			return null;
+		} else {
+			return new String(readByteArray(buffer), Charsets.UTF_8);
+		}
+	}
+
+	/**
+	 * Creates a byte array representation of the given String using UTF-8
+	 * encoding.
+	 * 
+	 * @param string
+	 *            the String that should be transformed into a byte array
+	 * @return the UTF-8 byte array of the given String
+	 */
+	public static byte[] writeString(String string) {
+		if (string == null) {
+			return new byte[0];
+		} else {
+			return string.getBytes(Charsets.UTF_8);
+		}
+	}
+
+	/**
+	 * Creates a byte array containing the serialized RDF model.
+	 * 
+	 * @param model
+	 *            the model that should be serialized
+	 * @return the byte array containing the model
+	 */
+	public static byte[] writeModel(Model model) {
+		if (model == null) {
+			return new byte[0];
+		} else {
+			StringWriter writer = new StringWriter();
+			RDFDataMgr.write(writer, model, DEFAULT_RDF_LANG);
+			return writeString(writer.toString());
+		}
+	}
 }
