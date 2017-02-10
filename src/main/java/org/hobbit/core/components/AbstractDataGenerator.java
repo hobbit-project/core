152a0295ef87b2ed83a2739aa54edf04f4034205
mroeder <roeder@informatik.uni-leipzig.de>
2017-02-10 18:44:39 +0100

    Added a better problem handling to the abstract data generator when it tries to check whether all sent data has been consumed.

--- src/main/java/org/hobbit/core/components/AbstractDataGenerator.java
+++ src/main/java/org/hobbit/core/components/AbstractDataGenerator.java
@@ -8,10 +8,15 @@ import org.apache.commons.io.IOUtils;
 import org.hobbit.core.Commands;
 import org.hobbit.core.Constants;
 import org.hobbit.core.data.RabbitQueue;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import com.rabbitmq.client.AlreadyClosedException;
 import com.rabbitmq.client.MessageProperties;
 
 public abstract class AbstractDataGenerator extends AbstractPlatformConnectorComponent {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(AbstractDataGenerator.class);
 
     private Semaphore startDataGenMutex = new Semaphore(0);
     private int generatorId;
@@ -25,29 +30,31 @@ public abstract class AbstractDataGenerator extends AbstractPlatformConnectorCom
         Map<String, String> env = System.getenv();
 
         if (!env.containsKey(Constants.GENERATOR_ID_KEY)) {
-            throw new IllegalArgumentException("Couldn't get \"" + Constants.GENERATOR_ID_KEY
-                    + "\" from the environment. Aborting.");
+            throw new IllegalArgumentException(
+                    "Couldn't get \"" + Constants.GENERATOR_ID_KEY + "\" from the environment. Aborting.");
         }
         try {
             generatorId = Integer.parseInt(env.get(Constants.GENERATOR_ID_KEY));
         } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("Couldn't get \"" + Constants.GENERATOR_ID_KEY
-                    + "\" from the environment. Aborting.", e);
+            throw new IllegalArgumentException(
+                    "Couldn't get \"" + Constants.GENERATOR_ID_KEY + "\" from the environment. Aborting.", e);
         }
 
         if (!env.containsKey(Constants.GENERATOR_COUNT_KEY)) {
-            throw new IllegalArgumentException("Couldn't get \"" + Constants.GENERATOR_COUNT_KEY
-                    + "\" from the environment. Aborting.");
+            throw new IllegalArgumentException(
+                    "Couldn't get \"" + Constants.GENERATOR_COUNT_KEY + "\" from the environment. Aborting.");
         }
         try {
             numberOfGenerators = Integer.parseInt(env.get(Constants.GENERATOR_COUNT_KEY));
         } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("Couldn't get \"" + Constants.GENERATOR_COUNT_KEY
-                    + "\" from the environment. Aborting.", e);
+            throw new IllegalArgumentException(
+                    "Couldn't get \"" + Constants.GENERATOR_COUNT_KEY + "\" from the environment. Aborting.", e);
         }
 
-        dataGen2TaskGenQueue = createDefaultRabbitQueue(generateSessionQueueName(Constants.DATA_GEN_2_TASK_GEN_QUEUE_NAME));
-        dataGen2SystemQueue = createDefaultRabbitQueue(generateSessionQueueName(Constants.DATA_GEN_2_SYSTEM_QUEUE_NAME));
+        dataGen2TaskGenQueue = createDefaultRabbitQueue(
+                generateSessionQueueName(Constants.DATA_GEN_2_TASK_GEN_QUEUE_NAME));
+        dataGen2SystemQueue = createDefaultRabbitQueue(
+                generateSessionQueueName(Constants.DATA_GEN_2_SYSTEM_QUEUE_NAME));
     }
 
     @Override
@@ -59,8 +66,25 @@ public abstract class AbstractDataGenerator extends AbstractPlatformConnectorCom
         generateData();
 
         // Unfortunately, we have to wait until all messages are consumed
-        while ((dataGen2TaskGenQueue.messageCount() + dataGen2SystemQueue.messageCount()) > 0) {
-            Thread.sleep(500);
+        try {
+            while (dataGen2SystemQueue.messageCount() > 0) {
+                Thread.sleep(500);
+            }
+        } catch (AlreadyClosedException e) {
+            LOGGER.info("The queue to the system is already closed. Assuming that all messages have been consumed.");
+        } catch (Exception e) {
+            LOGGER.warn("Exception while trying to check whether all messages have been consumed. It will be ignored.",
+                    e);
+        }
+        try {
+            while (dataGen2TaskGenQueue.messageCount() > 0) {
+                Thread.sleep(500);
+            }
+        } catch (AlreadyClosedException e) {
+            LOGGER.info("The queue to the system is already closed. Assuming that all messages have been consumed.");
+        } catch (Exception e) {
+            LOGGER.warn("Exception while trying to check whether all messages have been consumed. It will be ignored.",
+                    e);
         }
     }
 
@@ -81,8 +105,8 @@ public abstract class AbstractDataGenerator extends AbstractPlatformConnectorCom
     }
 
     protected void sendDataToSystemAdapter(byte[] data) throws IOException {
-        dataGen2SystemQueue.channel
-                .basicPublish("", dataGen2SystemQueue.name, MessageProperties.PERSISTENT_BASIC, data);
+        dataGen2SystemQueue.channel.basicPublish("", dataGen2SystemQueue.name, MessageProperties.PERSISTENT_BASIC,
+                data);
     }
 
     public int getGeneratorId() {
