b1045a1bf91616ff2a5fc5b79dc2a7690ebfcec4
mroeder <roeder@informatik.uni-leipzig.de>
2017-02-15 13:41:43 +0100

    Fixed a bug in the part that should wait for all messages to be processed of the AbstractTaskGenerator.

--- src/main/java/org/hobbit/core/components/AbstractTaskGenerator.java
+++ src/main/java/org/hobbit/core/components/AbstractTaskGenerator.java
@@ -208,7 +208,7 @@ public abstract class AbstractTaskGenerator extends AbstractCommandReceivingComp
             // available)");
             LOGGER.info("Waiting data processing to finish... ( {} / {} free permits are available)",
                     currentlyProcessedMessages.availablePermits(), maxParallelProcessedMsgs);
-            currentlyProcessedMessages.acquire(DEFAULT_MAX_PARALLEL_PROCESSED_MESSAGES);
+            currentlyProcessedMessages.acquire(maxParallelProcessedMsgs);
         }
     }
 
