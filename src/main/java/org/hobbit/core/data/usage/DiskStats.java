4b64229210741a70a0c4afdd67be585df740a789
Denis Kuchelev <denkv@users.noreply.github.com>
2019-12-07 23:09:17 +0100

    Add units in resource information docs

--- src/main/java/org/hobbit/core/data/usage/DiskStats.java
+++ src/main/java/org/hobbit/core/data/usage/DiskStats.java
@@ -1,26 +1,29 @@
 package org.hobbit.core.data.usage;
 
 public class DiskStats {
-    
+
     private long fsSizeSum = 0;
 
     public DiskStats() {
     }
 
+    /**
+     * @param fsSizeSum the sum of the disk usage to set (in bytes)
+     */
     public DiskStats(long fsSizeSum) {
         super();
         this.fsSizeSum = fsSizeSum;
     }
 
     /**
-     * @return the fsSizeSum
+     * @return the sum of the disk usage (in bytes)
      */
     public long getFsSizeSum() {
         return fsSizeSum;
     }
 
     /**
-     * @param fsSizeSum the fsSizeSum to set
+     * @param fsSizeSum the sum of the disk usage to set (in bytes)
      */
     public void setFsSizeSum(long fsSizeSum) {
         this.fsSizeSum = fsSizeSum;
